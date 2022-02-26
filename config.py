5f1b742c49ae67ec3d4172b651c8990611acd2b0
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-02-26 13:46:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,6 @@ class Config(object):
     
     API_HASH = environ.get("API_HASH", "")
     
-    channel_id = environ.get("CHANNEL_ID", None)
-    if ',' in auth_channel:
-        CHANNEL_ID = list(map(int, list(channel_id.split(','))))
-    else:
-        CHANNEL_ID = int(channel_id)
+    CHANNEL_ID = int(environ.get("CHANNEL_ID", None))
 
     MONGO_STR = environ.get("MONGO_STR", "")
